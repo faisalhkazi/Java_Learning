e696e250beee81466ba9423d67f65e8fbea8e2e8
faisalhkazi <faisalhkazi@gmail.com>
2024-09-16 22:09:31 +0530

    New Ex ception codes example

---
+++ Input/src/Exceptns/TestException2.java
@@ -0,0 +1,13 @@
+package Exceptns;
+
+public class TestException2 {
+
+    public static void main(String args[]) throws Exception
+    {
+        System.out.println("Program starts: ");
+        Thread.sleep(1000L);
+
+        System.out.println("Program awake: ");
+    }
+
+}
---
+++ Input/src/Exceptns/TestException3.java
@@ -0,0 +1,20 @@
+package Exceptns;
+
+class MyOwnException extends Exception 
+{
+    public MyOwnException (String msg)
+    {
+        super(msg);
+    }
+}
+
+public class TestException3 {
+
+    public static void main(String args[])
+    {
+        
+    }
+
+
+
+}
